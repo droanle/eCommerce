85b338172121276c634ab1ae88f088a57e558682
Leandro de Meirelles <meirellesoffice@gmail.com>
2022-09-18 10:41:56 -0300

    https error fixed

--- systemConfig.php
+++ systemConfig.php
@@ -8,11 +8,14 @@
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
     $url = "https://";
 else  
-    $url = "http://";   
+    $url = "http://";
 
 $url .= $_SERVER['HTTP_HOST'];
 
 if (file_exists("./localConfig.php")) include "./localConfig.php";
 else {
+    $url = "https://";
+    $url .= $_SERVER['HTTP_HOST'];
+    
     define("SYSTEM_URL", $url);
 }
