f74fd7b63f7afce628f06417d39a793d5d460598
Leandro de Meirelles <meirellesoffice@gmail.com>
2022-09-18 09:55:39 -0300

    update gitignore

--- localConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * * Se o sistema for executado localmente este arquivo será executado junto ao arquivo "systemConfig.php".
- * File: localConfig
- * Description: Estabelece definições e funções constantes no sistema quando é executado localmente.  
-*/
-
-define("ROUTER_EXCEPTION", "/eCommerce");
-define("SYSTEM_URL", $url . ROUTER_EXCEPTION);
